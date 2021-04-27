35d1f71e03f4c87da459ce2c0395d5b2e0e2c9aa
ampersandre <mougeot.andre@gmail.com>
2021-04-26 20:57:39 -0600

    Add serverless to nest-api

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/sample', '<rootDir>/apps/myapi'],
+  projects: [
+    '<rootDir>/apps/sample',
+    '<rootDir>/apps/myapi',
+    '<rootDir>/apps/nest-api',
+  ],
 };
